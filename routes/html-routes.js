da2202396cc814fbf93087be6dd5976aa3a9a949
Erika Vigliucci <evigliucci@gmail.com>
2020-05-22 22:08:38 -0400

    Converted html templates to handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,16 +11,17 @@ var path = require("path");
 module.exports = function(app) {
 
     // Each of the below routes just handles the HTML page that the user gets sent to.
+    //var capsule = require("../models/capsule.js");
 
     // index route loads view.html
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
+        res.render("index");
     });
 
-    // cms route loads cms.html
-    // app.get("/cms", function(req, res) {
-    //     res.sendFile(path.join(__dirname, "../public/cms.html"));
-    // });
+    //cms route loads cms.html
+    app.get("/login", function(req, res) {
+        res.render("login");
+    });
 
     // blog route loads blog.html
     // app.get("/blog", function(req, res) {
