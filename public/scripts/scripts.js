be49e46dcf984867ddc6516eb9b19df511cece9f
Erika Vigliucci <evigliucci@gmail.com>
2020-05-28 18:24:06 -0400

    in progress form reader

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -5,4 +5,19 @@ $(document).ready(function() {
             sideBySide: true
         });
     });
+
+    $("#saveAll").on("click", function(e) {
+        e.preventDefault();
+        var data_array = $("form").map(function() {
+            console.log($(this).serializeArray())
+            return $(this).serializeArray();
+        });
+        $.ajax({
+            url: 'api/capsuleSave',
+            data: data_array,
+            success: function(html) {
+
+            }
+        });
+    });
 });
