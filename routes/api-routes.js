52a261ec9a1a1d244774a4f9f9d17a058de6044d
James Hudeck <JamesWalterHudeck717@gmail.com>
2020-05-28 00:03:46 -0400

    created post method for movies
    Recieves input data from capsuleBuilder.handlebars.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,108 +1,147 @@
-var db = require("../models");
-var multer = require('multer');
-var upload = multer({ dest: 'uploads/' });
+let db = require("../models");
+let multer = require('multer');
+let upload = multer({ dest: 'uploads/' });
+const axios = require("axios");
 
-module.exports = function(app) {
+module.exports = function (app) {
     //POST = ADD = CREATE
     // =============================================================
 
     //ADD CAPSULES
-    app.post("/api/capsules", function(req, res) {
-        db.Capsule.create(req.body).then(function(dbCapsule) {
+    app.post("/api/capsules", function (req, res) {
+        db.Capsule.create(req.body).then(function (dbCapsule) {
             res.json(dbCapsule);
         });
     });
 
     //ADD FILES
-    app.post('/api/files', upload.single('myFile'), function(req, res, next) {
+    app.post('/api/files', upload.single('myFile'), function (req, res, next) {
         console.log(req.file);
     })
 
     //ADD USERS
-    app.post("/api/users", function(req, res) {
-        db.Author.create(req.body).then(function(dbUser) {
+    app.post("/api/users", function (req, res) {
+        db.Author.create(req.body).then(function (dbUser) {
             res.json(dbUser);
         });
     });
+    //Add Movies
+    app.post("/api/movies", function (req, res) {
+        let movie = req.body.movieInput;
+        // very close
+        res.status(200);
+    
+        const apiKey = "9b13178f";
+        let queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=" + apiKey;
+        console.log(queryURL);
+        axios.get(queryURL)
+           .then(function (res) {
+               console.log(res);
+            });
+    });
 
-    // GET = FIND = READ
-    // =============================================================
 
-    //FIND CAPSULES - DONT THINK WE WILL NEED THIS
-    app.get("/api/capsules", function(req, res) {
-        var query = {};
-        if (req.query.user_id) {
-            query.UserId = req.query.user_id;
+        //db.Capsule.create(req.body).then(function(dbCapsule) {
+        //res.json(dbCapsule);
+    //});
+
+
+//app.post("/api/movies", 
+
+// GET = FIND = READ
+// =============================================================
+
+//FIND CAPSULES - DONT THINK WE WILL NEED THIS
+app.get("/api/capsules", function (req, res) {
+    var query = {};
+    if (req.query.user_id) {
+        query.UserId = req.query.user_id;
+    }
+
+    db.Post.findAll({
+        where: query,
+        include: [db.User]
+    }).then(function (dbCapsule) {
+        res.json(dbCapsule);
+    });
+});
+
+//FIND SPECIFIC CAPSULE
+app.get("/api/capsules/:id", function (req, res) {
+
+    db.Capsule.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [db.file]
+    }).then(function (dbCapsule) {
+        res.json(dbCapsule);
+    });
+});
+
+//FIND SPECIFIC USER
+app.get("/api/users/:id", function (req, res) {
+    db.User.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [db.Capsule]
+    }).then(function (dbUser) {
+        res.json(dbUser);
+    });
+});
+
+app.get("/api/movies", function (req, res) {
+    let movie = res.data;
+
+    const apiKey = "9b13178f";
+    let queryUrl = "https://www.omdbapi.com/?t=" + movie + "&apikey=" + apiKey;
+    console.log(queryUrl);
+    //axios
+       // .get(queryURL)
+       // .then(function (res) {
+       //     console.log(res.data);
+        //});
+});
+
+
+
+
+// PUT = UPDATE
+// =============================================================
+
+//UPDATE SPECIFIC CAPSULE
+app.put("/api/capsules/:id", function (req, res) {
+    db.Capsule.update({
+        where: {
+            id: req.params.id
         }
-
-        db.Post.findAll({
-            where: query,
-            include: [db.User]
-        }).then(function(dbCapsule) {
-            res.json(dbCapsule);
-        });
+    }).then(function (dbCapsule) {
+        res.json(dbCapsule);
     });
+});
 
-    //FIND SPECIFIC CAPSULE
-    app.get("/api/capsules/:id", function(req, res) {
+// DELETE = DELETE
+// =============================================================
 
-        db.Capsule.findOne({
-            where: {
-                id: req.params.id
-            },
-            include: [db.file]
-        }).then(function(dbCapsule) {
-            res.json(dbCapsule);
-        });
+//DELETE SPECIFIC CAPSULE
+app.delete("/api/capsules/:id", function (req, res) {
+    db.Capsule.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(function (dbCapsule) {
+        res.json(dbCapsule);
     });
-
-    //FIND SPECIFIC USER
-    app.get("/api/users/:id", function(req, res) {
-        db.User.findOne({
-            where: {
-                id: req.params.id
-            },
-            include: [db.Capsule]
-        }).then(function(dbUser) {
-            res.json(dbUser);
-        });
+});
+//DELETE SPECIFIC USER
+app.delete("api.users/:id", function (req, res) {
+    db.User.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(function (dbUser) {
+        res.json(dbUser);
     });
-
-    // PUT = UPDATE
-    // =============================================================
-
-    //UPDATE SPECIFIC CAPSULE
-    app.put("/api/capsules/:id", function(req, res) {
-        db.Capsule.update({
-            where: {
-                id: req.params.id
-            }
-        }).then(function(dbCapsule) {
-            res.json(dbCapsule);
-        });
-    });
-
-    // DELETE = DELETE
-    // =============================================================
-
-    //DELETE SPECIFIC CAPSULE
-    app.delete("/api/capsules/:id", function(req, res) {
-        db.Capsule.destroy({
-            where: {
-                id: req.params.id
-            }
-        }).then(function(dbCapsule) {
-            res.json(dbCapsule);
-        });
-    });
-    //DELETE SPECIFIC USER
-    app.delete("api.users/:id", function(req, res) {
-        db.User.destroy({
-            where: {
-                id: req.params.id
-            }
-        }).then(function(dbUser) {
-            res.json(dbUser);
-        });
-    });
+});
 };
