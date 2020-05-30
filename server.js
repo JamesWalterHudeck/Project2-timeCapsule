47ebcec4b5ac4e3493f8e9c7cece24cb0f2eb74d
James Hudeck <JamesWalterHudeck717@gmail.com>
2020-05-30 12:12:27 -0400

    database no longer drops all

--- server.js
+++ server.js
@@ -26,11 +26,10 @@ app.set("view engine", "handlebars");
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
-
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-    app.listen(PORT, function() {
-        console.log("App listening on http://localhost:" + PORT);
-    });
+db.sequelize.sync({ force: false }).then(function () {
+  app.listen(PORT, function () {
+    console.log("App listening on http://localhost:" + PORT);
+  });
 });
